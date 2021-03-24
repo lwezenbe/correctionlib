d970c235693735fa4f0189638c7113801fd9df0e
IzaakWN <iwn_@hotmail.com>
2021-03-24 12:50:20 +0100

    improve documentation; add breakbrackets option

--- scripts/JSONEncoder.py
+++ scripts/JSONEncoder.py
@@ -7,13 +7,15 @@
 import json, math
 
 
-def write(data,fname,indent=2,maxlistlen=25,maxdictlen=2):
+def write(data,fname,indent=2,maxlistlen=25,maxdictlen=2,breakbrackets=False):
   """Help function to quickly write JSON file."""
   with open(fname,'w') as fout:
     if isinstance(data,dict):
-      fout.write(json.dumps(data,cls=JSONEncoder,sort_keys=True,indent=indent,maxlistlen=maxlistlen,maxdictlen=maxdictlen))
+      fout.write(json.dumps(data,cls=JSONEncoder,sort_keys=True,indent=indent,
+                 maxlistlen=maxlistlen,maxdictlen=maxdictlen,breakbrackets=breakbrackets))
     else:
-      fout.write(data.json(cls=JSONEncoder,exclude_unset=True,indent=indent,maxlistlen=maxlistlen,maxdictlen=maxdictlen))
+      fout.write(data.json(cls=JSONEncoder,exclude_unset=True,indent=indent,
+                 maxlistlen=maxlistlen,maxdictlen=maxdictlen,breakbrackets=breakbrackets))
   
 
 class JSONEncoder(json.JSONEncoder):
@@ -27,8 +29,9 @@ class JSONEncoder(json.JSONEncoder):
   """
   
   def __init__(self, *args, **kwargs):
-    self.maxdictlen = kwargs.pop('maxdictlen',25) # maximum of primitive elements per list, before breaking lines
-    self.maxlistlen = kwargs.pop('maxlistlen',25) # maximum of primitive elements per list, before breaking lines
+    self.maxlistlen    = kwargs.pop('maxlistlen',25) # maximum of primitive elements per list, before breaking lines
+    self.maxdictlen    = kwargs.pop('maxdictlen',25) # maximum of primitive elements per dict, before breaking lines
+    self.breakbrackets = kwargs.pop('breakbrackets',False) # break after opening bracket
     super(JSONEncoder,self).__init__(*args,**kwargs)
     self._indent = 0 # current indent
     self.parent = None # type of parent for recursive use
@@ -36,7 +39,7 @@ class JSONEncoder(json.JSONEncoder):
   def encode(self, obj):
     grandparent = self.parent
     self.parent = type(obj)
-    if isinstance(obj,(list,tuple)):
+    if isinstance(obj,(list,tuple)): # lists, tuples
       output = [ ]
       if all(isinstance(x,(int,float,str)) for x in obj): # list of primitives only
         if len(obj)>self.maxlistlen: # break long list into multiple lines
@@ -48,7 +51,7 @@ class JSONEncoder(json.JSONEncoder):
             for item in obj[i*maxlen:(i+1)*maxlen]:
               line.append(json.dumps(item))
             output.append(", ".join(line))
-          if grandparent==dict: # break first line
+          if grandparent==dict or self.breakbrackets: # break first line after opening bracket
             retval = "[\n"+indent_str+(",\n"+indent_str).join(output)+"\n"+' '*self._indent+"]"
           else: # do not break first line
             retval = "["+' '*(self.indent-1)+(",\n"+indent_str).join(output)+"\n"+' '*self._indent+"]"
@@ -64,26 +67,26 @@ class JSONEncoder(json.JSONEncoder):
         self._indent -= self.indent
         indent_str = " "*self._indent
         retval = "[\n"+",\n".join(output)+"\n"+indent_str+"]"
-    elif isinstance(obj,dict):
+    elif isinstance(obj,dict): # dictionaries
       output = [ ]
       if len(obj)<=self.maxdictlen and all(isinstance(obj[k],(int,float,str)) for k in obj): # write short dict on one line
         retval = "{ "+", ".join(json.dumps(k)+": "+self.encode(obj[k]) for k in obj)+" }"
       else: # break long dict into multiple line
         self._indent += self.indent
         indent_str = " "*self._indent
-        first = grandparent not in [None,dict]
+        first = grandparent not in [None,dict] and not self.breakbrackets # break after opening bracket
         for key, value in obj.items():
           valstr = self.encode(value)
-          if first and '\n' not in valstr: # no break on first
+          if first and '\n' not in valstr: # no break between opening bracket and first key
             row = ' '*(self.indent-1)+json.dumps(key)+": "+valstr
-          else:
+          else: # break before key
             row = '\n'+indent_str+json.dumps(key)+": "+valstr
           output.append(row)
           first = False
         self._indent -= self.indent
         indent_str = " "*self._indent
         retval = "{"+",".join(output)+"\n"+indent_str+"}"
-    else:
+    else: # use default formatting
       retval = json.dumps(obj)
     self.parent = grandparent
     return retval
@@ -123,7 +126,8 @@ if __name__ == '__main__':
     }
   }
   fname = "test_JSONEncoder.json"
-  print(json.dumps(data,cls=JSONEncoder,sort_keys=True,indent=2,maxlistlen=25,maxdictlen=3)) # print
+  print(json.dumps(data,cls=JSONEncoder,sort_keys=True,indent=2,
+                        maxlistlen=25,maxdictlen=3,breakbrackets=False)) # print
   print(f">>> Writing {fname}...")
   write(data,fname) # write
   print(f">>> Loading {fname}...")
